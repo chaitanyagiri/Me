980bad700807d25392591948608fb895a3447222
Chaitanya Giri <16ucc028@lnmiit.ac.in>
2017-06-23 13:13:45 +0530

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -1,15 +1,10 @@
 <?php
 $name       = $_POST['name']; 
-$from       = $_POST['email']; 
-$subject    = "Portfolio"; 
+$email       = $_POST['email']; 
+$subject    = "You got a message from your portfolio"; 
 $message    = $_POST['message']; 
 $to   		= 'girichaitanya11@gmail.com';
-$headers   = array();
-$headers[] = "MIME-Version: 1.0";
-$headers[] = "Content-type: text/plain; charset=iso-8859-1";
-$headers[] = "From: {$name} <{$from}>";
-$headers[] = "Reply-To: <{$from}>";
-$headers[] = "Subject: {$subject}";
-$headers[] = "X-Mailer: PHP/".phpversion();
-mail($to, $subject, $message) or die("Failure");
+ $mailBody="Name: $name\n Email: $email\n\n$message"; 
+
+ mail($to, $subject, $mailBody, "From: $name <$email>");
 ?>
